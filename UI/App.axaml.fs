87bc67a863bed8d31806ca603826f3663138a08a
Jamie050401 <jamieallen050401@gmail.com>
2024-02-03 09:58:52 +0000

    Added .NET solution

---
+++ UI/App.axaml.fs
@@ -0,0 +1,23 @@
+namespace UI
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Markup.Xaml
+open UI.ViewModels
+open UI.Views
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+            AvaloniaXamlLoader.Load(this)
+
+    override this.OnFrameworkInitializationCompleted() =
+
+
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktop ->
+             desktop.MainWindow <- MainWindow(DataContext = MainWindowViewModel())
+        | _ -> ()
+
+        base.OnFrameworkInitializationCompleted()
