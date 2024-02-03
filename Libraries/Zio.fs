da212ea4899109e550eece0472d521559d056111
Jamie050401 <jamieallen050401@gmail.com>
2024-02-03 10:18:06 +0000

    Added Libraries project

---
+++ Libraries/Zio.fs
@@ -0,0 +1,23 @@
+﻿namespace Libraries.Zio
+
+open Zio
+
+module Directory =
+    let create (path : UPath) (fs : IFileSystem)  =
+        fs.CreateDirectory path
+
+    let delete (isRecursive : bool) (path : UPath) (fs : IFileSystem) =
+        fs.DeleteDirectory (path, isRecursive)
+
+    let exists (path : UPath) (fs : IFileSystem) =
+        fs.DirectoryExists path
+
+module File =
+    let create (path : UPath) (fs : IFileSystem) =
+        fs.CreateFile path
+
+    let delete (path : UPath) (fs : IFileSystem) =
+        fs.DeleteFile path
+
+    let exists (path : UPath) (fs : IFileSystem) =
+        fs.FileExists path
